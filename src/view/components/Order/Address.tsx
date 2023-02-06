650f42fcbc2ba11477baa52aba74d19dc45803b8
Melvin <melvinjohner@hotmail.com>
2023-02-06 22:52:37 +0100

    implemented form validation

--- src/view/components/Order/Address.tsx
+++ src/view/components/Order/Address.tsx
@@ -1,112 +1,115 @@
 import React from "react";
 import {Container, Form, Stack} from "react-bootstrap";
-import {Formik} from "formik";
+import { useFormik} from "formik";
 import {number, object, string} from "yup";
+import {changeHandler} from "../OrderForm";
 
 const addressSchema = object({
-    firstName: string().required("Bitte Vornamen eingeben").max(50),
-    lastName: string().required("Bitte Nachnamen eingeben").max(50),
-    street: string().required("Bitte Strasse eingeben").max(50),
-    streetNr: string().required("Bitte Hausnummer eingeben").max(10),
-    zip: number().required("Bitte Postleitzahl eingeben").positive().integer().max(9999).min(1000),
-    city: string().required("Bitte Strasse eingeben").max(50),
+    firstName: string().required("Bitte Vornamen eingeben").max(50, "Dieses Feld darf nicht grösser sein als 50 Zeichen"),
+    lastName: string().required("Bitte Nachnamen eingeben").max(50, "Dieses Feld darf nicht grösser sein als 50 Zeichen"),
+    street: string().required("Bitte Strasse eingeben").max(50, "Dieses Feld darf nicht grösser sein als 50 Zeichen"),
+    streetNr: string().required("Bitte Hausnummer eingeben").max(10, "Dieses Feld darf nicht grösser sein als 10 Zeichen"),
+    zip: number().required("Bitte Postleitzahl eingeben").integer("Bitte eine Zahl eingeben").max(9999, "Die PLZ sollte unter 9999 sein").min(1000, "Die PLZ muss grösser als 1000 sein"),
+    city: string().required("Bitte Strasse eingeben").max(100, "Dieses Feld darf nicht grösser sein als 100 Zeichen"),
 });
 
-
 function Address(props: any) {
+    const formik = useFormik({
+
+        initialValues: {
+            firstName: props.formData.firstName,
+            lastName: props.formData.lastName,
+            street: props.formData.street,
+            streetNr: props.formData.streetNr,
+            zip: props.formData.zip,
+            city: props.formData.city
+        },
+        validationSchema: addressSchema,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+
+    })
 
     return (
         <Container>
+            <p className="text-start">Bitte hier angeben, an wen und wohin die Züpfen geliefert werden sollen.</p>
+            <Form onSubmit={formik.handleSubmit}>
 
-            <p className="text-start">Namen und Adresse</p>
-            <Formik initialValues={{
-                firstName: props.formData.firstName,
-                lastName: props.formData.lastName,
-                street: props.formData.street,
-                streetNr: props.formData.streetNr,
-                zip: props.formData.zip,
-                city: props.formData.city
-            }}
-                    validationSchema={addressSchema}
-                    onSubmit={async (values) => {
-                        alert(JSON.stringify(values, null, 2))}}>
-                {({
-                      handleSubmit,
-                      handleChange,
-                      handleBlur,
-                      values,
-                      touched,
-                      isValid,
-                      errors,
-                  }) =>
-                    (<Form>
-                        <Stack direction="horizontal" gap={2} className={"center"}>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Vorname</Form.Label>
-                                <Form.Control type="text"
-                                              name="firstname"
-                                              value={values.firstName}
-                                              onChange={handleChange}
-                                              isValid={touched.firstName && !errors.firstName}
+                <Stack direction="horizontal" gap={2} className={"center"}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Vorname</Form.Label>
+                        <Form.Control type="text"
+                                      name="firstName"
+                                      id="firstName"
+                                      value={formik.values.firstName}
+                                      onChange={e => changeHandler(e, formik, props)}
 
-                                ></Form.Control>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Nachname</Form.Label>
-                                <Form.Control type="text"
-                                              name="lastName"
-                                              value={values.lastName}
-                                              onChange={handleChange}
-                                              isValid={touched.lastName && !errors.lastName}
-                                ></Form.Control>
-                            </Form.Group>
-                        </Stack>
-                        <Stack direction="horizontal" gap={2} className={"center"}>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Strasse</Form.Label>
-                                <Form.Control type="text"
-                                              name="street"
-                                              value={values.street}
-                                              onChange={handleChange}
-                                              isValid={touched.street && !errors.street}
+                        ></Form.Control>
+                        <Form.Text>{formik.errors.firstName ?
+                            <div>{formik.errors.firstName as string}</div> : null}</Form.Text>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Nachname</Form.Label>
+                        <Form.Control type="text"
+                                      id="lastName"
+                                      name="lastName"
+                                      value={formik.values.lastName}
+                                      onChange={e => changeHandler(e, formik, props)}
+                        ></Form.Control>
+                        <Form.Text>{formik.errors.lastName ?
+                            <div>{formik.errors.lastName as string}</div> : null}</Form.Text>
+                    </Form.Group>
+                </Stack>
+                <Stack direction="horizontal" gap={2} className={"center"}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Strasse</Form.Label>
+                        <Form.Control type="text"
+                                      name="street"
+                                      id="street"
+                                      value={formik.values.street}
+                                      onChange={e => changeHandler(e, formik, props)}
+                        ></Form.Control>
+                        <Form.Text>{formik.errors.street ?
+                            <div>{formik.errors.street as string}</div> : null}</Form.Text>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Hausnummer</Form.Label>
+                        <Form.Control type="text"
+                                      name="streetNr"
+                                      id="streetNr"
+                                      value={formik.values.streetNr}
+                                      onChange={e => changeHandler(e, formik, props)}>
 
-                                ></Form.Control>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Hausnummer</Form.Label>
-                                <Form.Control type="text"
-                                              name="streetNr"
-                                              value={values.streetNr}
-                                              onChange={handleChange}
-                                              isValid={touched.streetNr && !errors.streetNr}>
+                        </Form.Control>
+                        <Form.Text>{formik.errors.streetNr ?
+                            <div>{formik.errors.streetNr as string}</div> : null}</Form.Text>
+                    </Form.Group>
+                </Stack>
+                <Stack direction="horizontal" gap={2} className={"center"}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>PLZ</Form.Label>
+                        <Form.Control type="number"
+                                      name="zip"
+                                      id="zip"
+                                      value={formik.values.zip}
+                                      onChange={e => changeHandler(e, formik, props)}>
 
-                                </Form.Control>
-                            </Form.Group>
-                        </Stack>
-                        <Stack direction="horizontal" gap={2} className={"center"}>
-                            <Form.Group className="mb-3">
-                                <Form.Label>PLZ</Form.Label>
-                                <Form.Control type="text"
-                                              name="zip"
-                                              value={values.zip}
-                                              onChange={handleChange}
-                                              isValid={touched.zip && !errors.zip}>
-
-                                </Form.Control>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Ort</Form.Label>
-                                <Form.Control type="text"
-                                              name="city"
-                                              value={values.city}
-                                              onChange={handleChange}
-                                              isValid={touched.city && !errors.city}>
-                                </Form.Control>
-                            </Form.Group>
-                        </Stack>
-
-                    </Form>)}
-            </Formik>
+                        </Form.Control>
+                        <Form.Text>{formik.errors.zip ? <div>{formik.errors.zip as string}</div> : null}</Form.Text>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Ort</Form.Label>
+                        <Form.Control type="text"
+                                      name="city"
+                                      id="city"
+                                      value={formik.values.city}
+                                      onChange={e => changeHandler(e, formik, props)}>
+                        </Form.Control>
+                        <Form.Text>{formik.errors.city ? <div>{formik.errors.city as string}</div> : null}</Form.Text>
+                    </Form.Group>
+                </Stack>
+            </Form>
 
         </Container>
     );
--- src/view/components/OrderForm.tsx
+++ src/view/components/OrderForm.tsx
@@ -1,35 +1,52 @@
 import {Button, Container, Stack} from "react-bootstrap";
-import Header from "./Header";
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import Intro from "./Order/Intro";
 import Address from "./Order/Address";
 
 
 function OrderForm() {
 
-    const [orderFromProgress, setOrderFromProgress] = useState(0);
-    const [formData, setFormData] = useState({})
+    const [OrderFormProgress, setOrderFormProgress] = useState(0);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName:  '',
+        street:  '',
+        streetNr:  '',
+        zip:  '',
+        city:  ''
+    })
     const orderForms = [
         <Intro></Intro>,
         <Address formData={formData} ></Address>
     ]
     return (
         <Container id="order" className={"border"}>
-            {orderForms[orderFromProgress]}
+            {orderForms[OrderFormProgress]}
 
             <Stack direction="horizontal" gap={2} className={"center"}>
-                {orderFromProgress > 0 ?
-                    <Button onClick={() => setOrderFromProgress(orderFromProgress - 1) } variant="primary" className={"center"}>
+                {OrderFormProgress > 0 ?
+                    <Button onClick={() => setOrderFormProgress(OrderFormProgress - 1) } variant="primary" className={"center"}>
                         Zurück
-                    </Button> : <div></div>}
-                {orderFromProgress < orderForms.length-1 ?
-                    <Button onClick={() => setOrderFromProgress(orderFromProgress + 1) } variant="primary" className={"center"}>
+                    </Button> : null}
+                {OrderFormProgress < orderForms.length-1 ?
+                    <Button onClick={() => setOrderFormProgress(OrderFormProgress + 1) } variant="primary" className={"center"}>
                         Weiter
-                    </Button> : <div></div>}
+                    </Button> : null}
             </Stack>
         </Container>
 
     );
+}
+export function changeHandler(e: ChangeEvent, formik: any, props: any): any {
+    const formikChange = formik.handleChange(e)
+    const {name, value} = e.currentTarget as HTMLInputElement;
+    console.log(formik.errors)
+    if (!formik.errors[name] === undefined) {
+        console.log(formik.errors[name])
+        props.formData[name] = value;
+    }
+    return formikChange;
 }
 
 export default OrderForm;
